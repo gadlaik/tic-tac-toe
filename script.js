7935d4a7932758c6afa27ea8df1489e4db1e799f
Mensur K <sabatonz@hotmail.com>
2021-04-05 21:41:28 +0200

    optimized script, added styles

--- script.js
+++ script.js
@@ -9,6 +9,49 @@ let Player = { wins: 0, score: document.querySelector(".player") };
 let AI = { wins: 0, score: document.querySelector(".opponent") };
 
 let Game = (function () {
+  let blur = document.querySelector(".content");
+  let cases = (one, two, three, xo) => {
+    if (
+      Gameboard.fields[one].textContent == xo &&
+      Gameboard.fields[two].textContent == xo &&
+      Gameboard.fields[three].textContent == xo
+    )
+      return true;
+  };
+  let stuff = () => {
+    Player.wins++;
+    blur.style.filter = "blur(5px)";
+    Gameboard.gameover = true;
+  };
+  let winCon = function (xo) {
+    switch (true) {
+      case cases(0, 1, 2, xo):
+        stuff();
+        break;
+      case cases(3, 4, 5, xo):
+        stuff();
+        break;
+      case cases(6, 7, 8, xo):
+        stuff();
+        break;
+      case cases(0, 4, 8, xo):
+        stuff();
+        break;
+      case cases(2, 4, 6, xo):
+        stuff();
+        break;
+      case cases(0, 3, 6, xo):
+        stuff();
+        break;
+      case cases(1, 4, 7, xo):
+        stuff();
+        break;
+      case cases(2, 5, 8, xo):
+        stuff();
+        break;
+    }
+  };
+
   return {
     ticTacToe: function () {
       Gameboard.gameboard.addEventListener("click", (e) => {
@@ -25,17 +68,12 @@ let Game = (function () {
             } else Gameboard.xo = "X";
           }
 
-          switch (true) {
-            case Gameboard.fields[0].textContent == "X" &&
-              Gameboard.fields[1].textContent == "X" &&
-              Gameboard.fields[2].textContent == "X":
-              Player.wins++;
-              Gameboard.gameover = true;
-              break;
-          }
+          winCon("X");
 
           Player.score.textContent = `Player: ${Player.wins}`;
+          AI.score.textContent = `Enemy: ${AI.wins}`;
         }
+
         if (Gameboard.gameover == true) {
           document.querySelector(".again").style.display = "block";
           document.querySelector(".again").addEventListener("click", (e) => {
@@ -43,6 +81,7 @@ let Game = (function () {
             Gameboard.fields.forEach((field) => (field.textContent = ""));
             document.querySelector(".again").style.display = "none";
             Gameboard.gameover = false;
+            blur.style.filter = "none";
           });
         }
       });
