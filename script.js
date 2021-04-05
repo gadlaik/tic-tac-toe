b0432b931c8d42b941c8e9de3acb2e388b88e4a8
Mensur K <sabatonz@hotmail.com>
2021-04-05 22:39:49 +0200

    Added draw, opponent script

--- script.js
+++ script.js
@@ -10,80 +10,104 @@ let AI = { wins: 0, score: document.querySelector(".opponent") };
 
 let Game = (function () {
   let blur = document.querySelector(".content");
-  let cases = (one, two, three, xo) => {
+  let cases = function (one, two, three, xo, ox) {
     if (
       Gameboard.fields[one].textContent == xo &&
       Gameboard.fields[two].textContent == xo &&
       Gameboard.fields[three].textContent == xo
-    )
+    ) {
+      Player.wins++;
       return true;
+    } else if (
+      Gameboard.fields[one].textContent == ox &&
+      Gameboard.fields[two].textContent == ox &&
+      Gameboard.fields[three].textContent == ox
+    ) {
+      AI.wins++;
+      return true;
+    }
   };
-  let stuff = () => {
-    Player.wins++;
+
+  let stuff = function () {
     blur.style.filter = "blur(5px)";
     Gameboard.gameover = true;
   };
-  let winCon = function (xo) {
+
+  let winCon = function (xo, ox) {
     switch (true) {
-      case cases(0, 1, 2, xo):
+      case cases(0, 1, 2, xo, ox):
         stuff();
         break;
-      case cases(3, 4, 5, xo):
+      case cases(3, 4, 5, xo, ox):
         stuff();
         break;
-      case cases(6, 7, 8, xo):
+      case cases(6, 7, 8, xo, ox):
         stuff();
         break;
-      case cases(0, 4, 8, xo):
+      case cases(0, 4, 8, xo, ox):
         stuff();
         break;
-      case cases(2, 4, 6, xo):
+      case cases(2, 4, 6, xo, ox):
         stuff();
         break;
-      case cases(0, 3, 6, xo):
+      case cases(0, 3, 6, xo, ox):
         stuff();
         break;
-      case cases(1, 4, 7, xo):
+      case cases(1, 4, 7, xo, ox):
         stuff();
         break;
-      case cases(2, 5, 8, xo):
+      case cases(2, 5, 8, xo, ox):
         stuff();
         break;
     }
   };
 
+  let gaming = function (e) {
+    if (e.target.classList.contains("field") && e.target.textContent == "") {
+      e.target.textContent = Gameboard.xo;
+
+      if (Gameboard.xo == "X") {
+        Gameboard.xo = "O";
+      } else Gameboard.xo = "X";
+    }
+  };
+
+  let gameIsOver = function () {
+    document.querySelector(".again").style.display = "block";
+    document.querySelector(".again").addEventListener("click", (e) => {
+      e.preventDefault();
+      Gameboard.fields.forEach((field) => (field.textContent = ""));
+      document.querySelector(".again").style.display = "none";
+      Gameboard.gameover = false;
+      blur.style.filter = "none";
+    });
+  };
+
   return {
     ticTacToe: function () {
       Gameboard.gameboard.addEventListener("click", (e) => {
+        let i = 0;
+
         if (Gameboard.gameover == false) {
-          e.preventDefault();
-          if (
-            e.target.classList.contains("field") &&
-            e.target.textContent == ""
-          ) {
-            e.target.textContent = Gameboard.xo;
+          gaming(e);
 
-            if (Gameboard.xo == "X") {
-              Gameboard.xo = "O";
-            } else Gameboard.xo = "X";
+          // win or draw
+          if (winCon("X", "O") == true) return;
+          else {
+            Gameboard.fields.forEach((field) => {
+              if (field.textContent != "") i++;
+              if (i == 9) {
+                Gameboard.gameover = true;
+                blur.style.filter = "blur(5px)";
+              }
+            });
           }
-
-          winCon("X");
 
           Player.score.textContent = `Player: ${Player.wins}`;
           AI.score.textContent = `Enemy: ${AI.wins}`;
         }
 
-        if (Gameboard.gameover == true) {
-          document.querySelector(".again").style.display = "block";
-          document.querySelector(".again").addEventListener("click", (e) => {
-            e.preventDefault();
-            Gameboard.fields.forEach((field) => (field.textContent = ""));
-            document.querySelector(".again").style.display = "none";
-            Gameboard.gameover = false;
-            blur.style.filter = "none";
-          });
-        }
+        if (Gameboard.gameover == true) gameIsOver();
       });
     },
   };
